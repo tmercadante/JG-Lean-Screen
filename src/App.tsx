6b550123fa88471552c7b5edeff5e21ef769cf77
Tony Mercadante <tmercadante@jahnelgroup.com>
2025-09-17 11:53:58 -0400

    render ThemeToggle globally

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
+import { ThemeToggle } from "./components/ThemeToggle";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
@@ -19,6 +20,7 @@ const App = () => (
         <TooltipProvider>
           <Toaster />
           <Sonner />
+          <ThemeToggle />
           <BrowserRouter>
             <Routes>
               <Route path="/" element={<Home />} />
