7d7af960d278962d4afb8ae3e4c9fae87b006d78
Tony Mercadante <tmercadante@jahnelgroup.com>
2025-09-17 12:01:20 -0400

    removed brackets

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
-import { ThemeToggle } from "./components/ThemeToggle";
+import ThemeToggle from "./components/ThemeToggle";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
